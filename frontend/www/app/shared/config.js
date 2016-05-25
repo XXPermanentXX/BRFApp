8f042e8ea6ca62edccc11371e50525b0fe5eedd5
isaacjrs <isaac.jr.sosa@gmail.com>
2016-05-25 19:33:08 +0200

    Config adjustments

--- frontend/www/app/shared/config.js
+++ frontend/www/app/shared/config.js
@@ -3,6 +3,7 @@ angular.module('civis.youpower')
 .factory('Config', function() {
   return {
      // host: 'http://localhost:3000'
-     host: 'https://app.civisproject.eu'
+     //host: 'https://app.civisproject.eu'
+    host:'http://civis.proj.kth.se'
   }
 });
