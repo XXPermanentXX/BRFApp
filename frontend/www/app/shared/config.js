b40369dcc24d8ca19f47c24c82186ee911406a39
cristi <cristi@kth.se>
2016-12-02 14:57:04 +0100

    using localhost by default rather than app.civis.eu server

--- frontend/www/app/shared/config.js
+++ frontend/www/app/shared/config.js
@@ -2,8 +2,8 @@ angular.module('civis.youpower')
 
 .factory('Config', function() {
   return {
-    //host: 'http://localhost:3000'
+    host: 'http://localhost:3000'
     //host: 'https://app.civisproject.eu'
-     host:'http://civis.proj.kth.se'
+    // host:'http://civis.proj.kth.se'
   }
 });
