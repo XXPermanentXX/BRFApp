eb72c3818fa2b1a010587a20eeaad7d28cf5e85d
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-02-16 15:30:59 +0100

    Add energy action summary

--- backend/views/cooperative.js
+++ backend/views/cooperative.js
@@ -9,6 +9,17 @@ module.exports = function (state, prev, send) {
       <div class="App-container">
         <h1 class="App-title">${ state.name }</h1>
         ${ performance({ performance: state.performance }) }
+        <hr class="u-marginVm" />
+        <div class="u-flex u-flexJustifyCenter u-marginVm u-styleItalic">
+          ${ state.actions.length ? html`
+            <div>
+              <span class="u-floatLeft u-sizeG u-marginRb">${ state.actions.length }</span>
+              <span class="u-sizeL">Energy actions</span>
+              <br />
+              <a href="#energy-actions">Show</a>
+            </div>
+          ` : html`<span class="u-sizeL">No energy actions</span>` }
+        </div>
       </div>
     </div>
   `;
