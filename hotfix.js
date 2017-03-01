deb8272ade9cf6a564cea95bf80ced9ef04ae339
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-03-01 11:21:07 +0100

    Add hot reload of views, pages and components

---
+++ hotfix.js
@@ -0,0 +1,16 @@
+require('hotswap');
+
+const orig = require.extensions['.js'];
+
+require.extensions['.js'] = function (m, filename) {
+  if (/backend\/(views|components|pages)/.test(filename)) {
+    const _compile = m._compile;
+
+    m._compile = function (content, filename) {
+      content += ';module.change_code = 1;';
+      return _compile.call(this, content, filename);
+    };
+  }
+
+  orig(m, filename);
+};
