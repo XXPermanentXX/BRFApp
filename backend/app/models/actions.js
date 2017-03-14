12acec17723339f2a8611a8b478ad88396ec030b
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-03-14 10:55:39 +0100

    Add action fetch effect

--- backend/app/models/actions.js
+++ backend/app/models/actions.js
@@ -2,9 +2,47 @@ module.exports = function actions(state = {}) {
   return {
     namespace: 'actions',
     reducers: {
+      add(state, data) {
+        const items = state.items.splice();
+
+        if (Array.isArray(data)) {
+          items.push(...data);
+        } else {
+          items.push(data);
+        }
+
+        return Object.assign({}, state, { items });
+      }
+    },
+    effects: {
+      fetch(state, options, send, done) {
+        if (Array.isArray(options)) {
+          Promise.all(options.map(fetchAction)).then(results => {
+            send('actions:add', results, done);
+          }, done);
+        } else {
+          fetchAction(options).then(result => {
+            send('actions:add', result, done);
+          }, done);
+        }
+      }
     },
     state: Object.assign({
-      items: [],
+      items: []
     }, state)
   };
 };
+
+/**
+ * Fetch action by id
+ * @param  {String} id Unique id for action to fetch
+ * @return {Promise}   Resolves to actions data
+ */
+
+function fetchAction(id) {
+  return fetch(
+    `/actions/${ id }`,
+    { headers: { accept: 'application/json' }}
+  )
+  .then(body => body.json());
+}
