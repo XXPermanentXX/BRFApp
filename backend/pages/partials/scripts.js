5f7392ef12529d554526dfb8f6ae62dbe50eebb4
Carl Törnqvist <calle.tornqvist@gmail.com>
2017-03-18 16:55:43 +0100

    Add polyfill service

--- backend/pages/partials/scripts.js
+++ backend/pages/partials/scripts.js
@@ -1,8 +1,14 @@
 const html = require('choo/html');
 
-module.exports = function (state) {
-  return [
+module.exports = function scrips(state) {
+  const tags = [
     html`<script type="application/json" class="js-initialState">${ JSON.stringify(state) }</script>`,
-    html`<script src="/index.js"></script>`
+    html`<script src="/index.js" async></script>`
   ];
+
+  if (process.env.NODE_ENV !== 'development') {
+    tags.unshift(html`<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch"></script>`);
+  }
+
+  return tags;
 };
