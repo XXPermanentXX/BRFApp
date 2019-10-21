60c68a0a8ad5e5d95bec37bebbc944aa1294bbfa
Carl Törnqvist <carl@tornqv.ist>
2019-10-21 12:22:18 +0200

    Lintage

--- components/icons/symbols.js
+++ components/icons/symbols.js
@@ -10,7 +10,7 @@ module.exports = class Symbols extends Component {
     return html`
       <div class="u-hidden">
         <svg>
-          <symbol width="10px" height="16px" viewBox="-2 -8 10 16" id="icon-chevron">
+          <symbol viewBox="-2 -8 10 16" id="icon-chevron">
             <g>
               <polygon stroke="none" fill="currentColor" fill-rule="nonzero" points="7.20997244 -5.79575887 5.79575887 -7.20997244 -1.61897814 0.20476458 5.79575887 7.6195016 7.20997244 6.20528803 1.20944898 0.20476458" />
             </g>
